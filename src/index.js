da5c6d27baa5cb47e8a7d451b3215f49889a05e6
Nancy Zaky <nancyzki@yahoo.com>
2021-07-09 20:42:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,91 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const toysForm = document.querySelector(".add-toy-form");
+    const toyName = toysForm.name.value;
+    const newToyUrl = toysForm.image.value;
+    const newToyObj = {
+      name: toyName,
+      image: newToyUrl,
+      likes: 0,
+    };
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(newToyObj),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        return error.value;
+      });
+  });
+  fetch("http://localhost:3000/toys")
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      let divContainer = document.getElementById("toy-collection");
+
+      data.forEach((item) => {
+        console.log(item);
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+
+        let header = document.createElement("h2");
+        header.innerText = item.name;
+        let imageUrl = document.createElement("img");
+        imageUrl.classList.add("toy-avatar");
+        imageUrl.src = item.image;
+
+        let paragraph = document.createElement("p");
+        let likesCount = item.likes;
+
+        paragraph.innerText = `${likesCount} likes`;
+        let likeButton = document.createElement("button");
+        likeButton.classList.add("like-btn");
+        likeButton.id = item.id;
+        likeButton.innerText = "Like";
+        likeButton.addEventListener("click", (e) => {
+          item.likes++;
+          e.preventDefault();
+          console.log(e.target);
+          fetchUrl = `http://localhost:3000/toys/${item.id}`;
+
+          fetch(fetchUrl, {
+            method: "PATCH",
+            headers: {
+              "Content-type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({ likes: item.likes + 1 }),
+          })
+            .then((resp) => resp.json())
+            .then((data) => {
+              console.log(data);
+              paragraph.innerText = `${data.likes} likes`;
+            });
+        });
+
+        cardDiv.appendChild(header);
+        cardDiv.appendChild(imageUrl);
+        cardDiv.appendChild(paragraph);
+        cardDiv.appendChild(likeButton);
+        divContainer.appendChild(cardDiv);
+      });
+    });
 });
+
+//
+
+//   let fetchUrl = `http://localhost:3000/toys/${likeButton.id}`;
+//   console.log(fetchUrl);
+//
+// ;
